ea7edffdd73146c83023b013adda23c9321d358d
leeh697 <leeh697@newschool.edu>
2023-11-16 02:07:13 -0500

    entry 9
    this is a commit

---
+++ harmoniccollection/entry9/main.js
@@ -0,0 +1,6 @@
+const card = document.querySelector(".card_inner");
+card.addEventListener("click",function(e){
+	card.classList.toggle('is-flipped');
+});
+
+alert('Have you finished the assignments?!')
